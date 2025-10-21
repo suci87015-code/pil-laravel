28357b0b1e20bc0c28d6436ed03200cbab799deb
Suci <suci87015@gmail.com>
2025-10-21 20:20:11 +0700

    PUSH pertama saya untuk project laravel kedalam github

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+   return view('index');
+});
+
+Route::get('about',function(){
+    return view('about');
+});
+
+Route::get('mahasiswa',function(){
+    return view('mahasiswa');
+});
+
+Route::get('profile',function(){
+    $nama = 'Suci';
+    // return view('profile', compact('nama'));
+    return view('profile')->with('nama', $nama);
+});
