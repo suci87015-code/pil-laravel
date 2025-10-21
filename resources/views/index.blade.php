89a045504b072a121fddefdc0cf8641a7c458f40
Suci <suci87015@gmail.com>
2025-10-21 21:08:01 +0700

    update file index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,13 +35,10 @@
   </div>
 </nav>
 
+<div class="container">
     <h1>Ini adalah halaman Home</h1>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/mahasiswa">Mahasiswa</a></li>
-        <li><a href="/profile">Profile</a></li>
-    </ul>
+</div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
